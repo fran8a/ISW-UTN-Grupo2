30b5751849f694118a1b4e264a7d2818bfac77a2
Fabrizio Torres Daniele <94231@sistemas.frc.utn.edu.ar>
2025-10-26 13:00:15 -0300

    feat: se agregan variables de entorno

--- InstanciasEvaluativas/TrabajosPracticos/TrabajoPractico6/Entregas/frontend/vite.config.ts
+++ InstanciasEvaluativas/TrabajosPracticos/TrabajoPractico6/Entregas/frontend/vite.config.ts
@@ -1,28 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), ""); 
 
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+  const apiUrl = env.VITE_BACKEND_URL;
+  console.log(`API URL: ${apiUrl}`);
+
+  return {
+    server: {
+      host: "::",
+      port: 8080,
+      proxy: {
+        "/api": {
+          target: apiUrl,           
+          changeOrigin: true,
+          rewrite: (p) => p.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: { "@": path.resolve(__dirname, "./src") },
     },
-  },
-}));
+  };
+});
